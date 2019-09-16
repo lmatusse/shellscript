84fa511b075cca50e07289b71aa35f0827a5587e
lmatusse <55313994+lmatusse@users.noreply.github.com>
2019-09-16 11:47:50 +0200

    Criando o meu primeiro programa em CSharp
    Criando o meu primeiro programa em CSharp

---
+++ teste01.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Projecto1
+{
+    class Teste1
+    {
+    
+        static void Main(string[] args) 
+        {
+           Console.WriteLine("Laila Amosse Matusse");
+           Console.ReadKey();
+        }
+    }
+}
